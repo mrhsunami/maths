9e14d941e91784a5be0d665fdfce101500115918
Nathan <nathan.hsu@icloud.com>
2018-02-13 18:12:23 -0800

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Nathan Hsu on 2018-02-13.
+//  Copyright © 2018 Nathan Hsu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
